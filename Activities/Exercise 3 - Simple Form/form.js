638edad884f48bf59e47aaf54bc84b8140ef2c05
birchjo <100217824+birchjo@users.noreply.github.com>
2024-01-15 17:14:47 -0500

    Add Exercise 3 activity

---
+++ Activities/Exercise 3 - Simple Form/form.js
@@ -0,0 +1,32 @@
+(function () {
+  const form_id = "contact_form";
+  const form_click_id = "form_submit";
+  const $form = document.querySelector(`#${form_id}`);
+  const $click_submit = document.querySelector(`#${form_click_id}`);
+  const $input = document.querySelectorAll(
+    `#${form_id} input[type=text], #${form_id} input[type=email], #${form_id} input:checked`
+  );
+
+  let getFormData = function () {
+    let data = [];
+
+    $input.forEach(function (item, index) {
+      data.push(item.value);
+    });
+
+    return data;
+  };
+
+  // Request submission
+  $click_submit.addEventListener("click", function (e) {
+    $form.requestSubmit();
+  });
+
+  // Handle submission
+  $form.addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent form from submitting
+    let form_data = getFormData();
+
+    console.log("Form submitted!", form_data);
+  });
+})();
